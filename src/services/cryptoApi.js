e730a7aa29d8291af8d442f900d121bcd6b0e745
Mehdi Barim <60230244+DroidBarber@users.noreply.github.com>
2021-10-01 19:48:46 +0200

    first

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '8444295572msh24f383cf9a09919p1c81f7jsn2d7922cb974f'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+        })
+    })
+});
+
+export const { 
+    useGetCryptosQuery,
+    useGetExchangesQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery
+ } = cryptoApi;
